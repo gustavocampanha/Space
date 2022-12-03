c7b16cc609d7ebcf74972896b73dcee1522d08a3
AlmirFonseca <almirfonseca60@gmail.com>
2022-12-03 17:49:40 -0300

    Commit automático

---
+++ main.py
@@ -0,0 +1,10 @@
+"""Esse é o módulo principal, ele será executado para exibir o jogo.
+"""
+
+# Importe a biblioteca do jogo
+
+from src.space_oddity import Game
+
+# Inicializa o jogo
+Game()
+                           
